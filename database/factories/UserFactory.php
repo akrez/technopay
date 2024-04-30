f7bb96c67d6b7e9ef8b78971918400d60cd745ba
Akrez <akrez.like@gmail.com>
2024-05-01 00:35:41 +0330

    UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'national_code' => fake()->numerify('##########'),
+            'mobile' => fake()->numerify('09#########'),
         ];
     }
 
