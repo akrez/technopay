5d95714630b100bbde795a8dafd1e524cf397036
Akrez <akrez.like@gmail.com>
2024-05-01 00:57:20 +0330

    order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -11,8 +12,22 @@ class Order extends Model
 {
     use HasFactory, SoftDeletes;
 
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeFilterNull(Builder $query, $column, $value, $operator = '=')
+    {
+        $query->when(!is_null($value), function (Builder $query) use ($column, $operator, $value) {
+            $query->where($column, $operator, $value);
+        });
     }
 }
