4e9983c687f0870ef05bbffa462b5609091794de
Akrez <akrez.like@gmail.com>
2024-05-01 00:48:26 +0330

    DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'admin',
+            'national_code' => '0010000000',
+            'email' => config('technopay.ADMIN_EMAIL'),
+            'mobile' => config('technopay.ADMIN_MOBILE'),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            OrderSeeder::class,
+        ]);
     }
 }
