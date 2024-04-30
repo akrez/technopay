d4c224720365f8db66039d11454698d0b76ab088
Akrez <akrez.like@gmail.com>
2024-04-30 21:00:58 +0330

    make:migration create_orders_table

---
+++ database/migrations/2024_04_30_172222_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Enums\Order\OrderStatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', [OrderStatusEnum::ACTIVE->value, OrderStatusEnum::DEACTIVE->value]);
+            $table->decimal('amount');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
