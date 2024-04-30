23220fca8d152260f4ebda27133b16c2ede59c2d
Akrez <akrez.like@gmail.com>
2024-05-01 01:15:15 +0330

    ResponseService

---
+++ app/Services/ResponseService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Support\Facades\Lang;
+
+class ResponseService implements Responsable
+{
+    public int $status = 200;
+    public string $message = 'OK';
+    public mixed $data;
+    public array $errors = [];
+
+    public function status(int $status): ResponseService
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function message(string $message): ResponseService
+    {
+        $this->message = (Lang::has($message) ? __($message) : $message);
+        return $this;
+    }
+
+    public function data($data): ResponseService
+    {
+        $this->data = $data;
+        return $this;
+    }
+
+    public function errors($errors): ResponseService
+    {
+        $this->errors = is_array($errors) ? $errors : [$errors];
+        return $this;
+    }
+
+    public function toResponse($request)
+    {
+        return response([
+            'message' => $this->message,
+            'data' => $this->data,
+            'errors' => $this->errors,
+        ], $this->status);
+    }
+}
