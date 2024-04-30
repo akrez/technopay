2d0b2507085113bd7d9f06c6d38d7ca6540218da
Akrez <akrez.like@gmail.com>
2024-05-01 02:04:43 +0330

    MelipayamakChannel

---
+++ app/Channels/MelipayamakChannel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Channels;
+
+use \Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class MelipayamakChannel
+{
+    /**
+     * Send the given notification.
+     *
+     * @param  mixed  $notifiable
+     * @param  \Illuminate\Notifications\Notification  $notification
+     * @return void
+     */
+    public function send($notifiable, Notification $notification)
+    {
+        Http::fake(function ($request) use ($notification) {
+            return Http::response(json_encode(['status' => true]), 200, [
+                'Content-Type' => 'application/json',
+            ]);
+        });
+
+        $response = Http::get('https://api.payamak-panel.com/post/Send.asmx/SendSimpleSMS', [
+            'message' => $notification->message,
+            'trace' => $notification->trace,
+        ]);
+
+        Log::info($response->body());
+    }
+}
